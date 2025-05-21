1b1a66923832a04c92d978019864231810a83bc4
Heclypso <heclypso@gmail.com>
2025-05-20 21:23:31 -0300

    Praticado importação e exportação com React

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import {PI, GTM_BRASIL} from './teste'
+import nomes from './nomes'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {PI}
+    {GTM_BRASIL}
+    {nomes}
     <App />
   </StrictMode>,
 )
---
+++ src/nomes.js
@@ -0,0 +1,3 @@
+const nomes = ['Gabriel, Gustavo, Carlos']
+
+export default nomes
